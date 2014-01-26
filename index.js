8e71231777eb2f0389cc8613af6d4c95ac255f8a
Dan Rowe <draco2003@gmail.com>
2014-01-25 23:48:46 -0500

    add project config files, and pet jshint

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+'use strict';
+
 var cluster = require('cluster')
+  , checksum = require('checksum')
   , debug = require('debug')('hashbot:cluster')
   , conf = require('nconf')
   , env = process.env.NODE_ENV || 'development';
@@ -8,13 +11,13 @@ conf.add('all', { type: 'file', file: __dirname + '/config/global.json' });
 
 //For testing you can use this to fill the hashbox with hashes.
 //var hashbox_url = nconf.get("hashbox:host") + nconf.get("create");
-var hashbox_url = conf.get("hashbox:host") + conf.get("hashbox:verify");
+var hashboxURL = conf.get('hashbox:host') + conf.get('hashbox:verify');
 
 if (cluster.isMaster) {
   var scandir = require('scandir').create()
     , files = [];
 
-  scandir.on('file', function(file, stats) {
+  scandir.on('file', function(file) {
     files.push(file);
   });
 
@@ -24,7 +27,7 @@ if (cluster.isMaster) {
 
   scandir.on('end', function(){
     // Start workers and listen for messages containing notifyRequest
-    var numWorkers = conf.get("hash:threads") || require('os').cpus().length;
+    var numWorkers = conf.get('hash:threads') || require('os').cpus().length;
     for (var i = 0; i < numWorkers; i++) {
       cluster.fork();
     }
@@ -32,24 +35,24 @@ if (cluster.isMaster) {
     // If enabled display a progress status to the console
     if (conf.get('progress:display')) {
       var interval = conf.get('progress:interval') || 1000;
-      console.log("files to process", files.length);
+      console.log('files to process', files.length);
       setInterval(function() {
-        var files_left = files.length;
-        if (files_left === 0) {
+        var filesLeft = files.length;
+        if (filesLeft === 0) {
           clearInterval(this);
         } else {
-          console.log("files remaining", files.length);
+          console.log('files remaining', files.length);
         }
       }, interval);
     }
 
     Object.keys(cluster.workers).forEach(function(id) {
-      cluster.workers[id].on('message', function(worker) {
+      cluster.workers[id].on('message', function() {
         if (files.length > 0) {
-          var next_file = files.pop();
+          var nextFile = files.pop();
           cluster.workers[id].send({
             cmd: 'hashFile',
-            data: next_file
+            data: nextFile
           });
         } else {
           //Disconnect the workers after we are done processing all files
@@ -69,49 +72,41 @@ if (cluster.isMaster) {
 
 } else {
   // Only the works need these and this creates a new request pool per worker.
-  var checksum = require('checksum')
-    , request = require('request');
+  var request = require('request');
 
-  process.send({cmd: "ready", msg: "ready"});
+  process.send({cmd: 'ready', msg: 'ready'});
   process.on('message', function(msg) {
     if (msg.cmd === 'hashFile') {
-      process_file(msg.data, function(err, hash) {
+      processFile(msg.data, function(err, hash) {
         if (err) {
           console.log(err);
         } else {
           debug(JSON.stringify(hash));
-          send_results(hash, function(err, response) {
-            if (err) {
-              //console.log(err);
-            }
-          });
+          request.post(hashboxURL, { form:hash },
+            function (err, response, body) {
+              if (err) {
+                console.log(err);
+                console.log(response);
+                console.log(body);
+              }
+              process.send({cmd: 'ready', msg: 'ready'});
+            });
         }
-        process.send({cmd: "ready", msg: "ready"});
       });
     }
   });
+}
 
-  function process_file(file, callback) {
-    debug('file:' + file);
-    var options = {
-      algorithm: conf.get('hash:algorithm') || 'sha1'
-    };
+function processFile(file, callback) {
+  debug('file:' + file);
+  var options = {
+    algorithm: conf.get('hash:algorithm') || 'sha1'
+  };
 
-    checksum.file(file, options, function (err, hash) {
-      if (err) {
-        console.log(err);
-      }
-      callback(err, {key: file, hash: hash});
-    });
-  }
-
-  function send_results(hash, callback) {
-    request.post(hashbox_url, { form:hash },
-      function (err, response, body) {
-        if (err) {
-          console.log(err);
-        }
-        callback(err, response.toJSON());
-    });
-  }
+  checksum.file(file, options, function (err, hash) {
+    if (err) {
+      console.log(err);
+    }
+    callback(err, {key: file, hash: hash});
+  });
 }
